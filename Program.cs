1be9169c2f98a6de09e3b720a2e153bda2748399
SurveshRathore <educatedevilboy@gmail.com>
2022-10-14 14:33:44 +0530

    Master program

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day3LineComparison;
+Console.WriteLine("Welcome to Line Comparison Computation Program ");
+
+UC1LineLength uc1ll = new();
+uc1ll.calcLineLength(12,6,8,9);
+
+
+
