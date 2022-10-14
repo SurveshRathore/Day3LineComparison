3e7bf1710ca75cc12157a8aed3ee339dfa4d9715
SurveshRathore <educatedevilboy@gmail.com>
2022-10-14 14:35:25 +0530

    UC1 Length of a line

---
+++ UC1LineLength.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3LineComparison
+{
+    internal class UC1LineLength
+    {
+        public void calcLineLength (double x1, double y1, double x2, double y2)
+        {
+            double lineLength;
+            lineLength = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("The length of a line is: " + lineLength);
+        }
+    }
+}
